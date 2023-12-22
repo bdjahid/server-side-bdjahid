68a896be655b5f4cc8f6af176c7117f3727a986e
Your Name <you@example.com>
2023-12-22 21:33:29 +0600

    booking complete

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ async function run() {
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking)
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result)
         })
 
 
