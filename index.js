6d91de75a5422673e1b168930ee4205437da0ec4
Your Name <you@example.com>
2023-12-24 18:13:12 +0600

    query

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+// use for token
+// const corsConfig = {
+//     origin: ['https://b8a11-client-side-bdjahid.web.app'],
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE']
+// }
+app.use(cors())
 app.use(express.json())
 
 
@@ -28,98 +33,13 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-
-        const serviceCollection = client.db('tours-guide').collection('services')
-        const bookingCollection = client.db('tours-guide').collection('bookings')
-        const productCollection = client.db('tours-guide').collection('product')
-
-        app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find();
-            const result = await cursor.toArray();
-            res.send(result)
-        })
-
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await serviceCollection.findOne(query)
-            res.send(result)
-        })
-
-        // add product
-
-        app.get('/product', async (req, res) => {
-            const cursor = productCollection.find();
-            const result = await cursor.toArray();
-            res.send(result)
-        })
-        app.get('/product/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id)
-            const query = { _id: new ObjectId(id) }
-            const result = await productCollection.findOne(query)
-            res.send(result)
-        })
-
-        app.post('/product', async (req, res) => {
-            const product = req.body;
-            console.log(product)
-            const result = await productCollection.insertOne(product);
-            res.send(result)
-        })
-
-        app.put('/product/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id)
-            const filter = { _id: new ObjectId(id) }
-            const options = { upsert: true };
-            const updateService = req.body;
-            const service = {
-                $set: {
-                    area: updateService.area,
-                    description: updateService.description,
-                    photo: updateService.photo,
-                    price: updateService.price,
-                    service: updateService.service
-                }
-            }
-            const result = await productCollection.updateOne(filter, service, options)
-            res.send(result)
-        })
-
-        app.delete('/product/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id)
-            const query = { _id: new ObjectId(id) }
-            const result = await productCollection.deleteOne(query)
-            res.send(result)
-        })
-
-        // post bookings
-
-        app.get('/bookings', async (req, res) => {
-            console.log(req.query.email);
-            let query = {}
-            if (req.query?.email) {
-                query = { email: req.query.email }
-            }
-            const result = await bookingCollection.find(query).toArray();
-            res.send(result)
-        })
-
-        app.post('/bookings', async (req, res) => {
-            const booking = req.body;
-            console.log(booking)
-            const result = await bookingCollection.insertOne(booking);
-            res.send(result)
-        })
+        client.connect();
 
 
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
@@ -128,8 +48,96 @@ async function run() {
 }
 run().catch(console.dir);
 
+const serviceCollection = client.db('tours-guide').collection('services')
+const bookingCollection = client.db('tours-guide').collection('bookings')
+const productCollection = client.db('tours-guide').collection('product')
+
+app.get('/services', async (req, res) => {
+    const cursor = serviceCollection.find();
+    const result = await cursor.toArray();
+    res.send(result)
+})
 
 
+app.get('/services/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await serviceCollection.findOne(query)
+    res.send(result)
+})
+
+// add product
+
+app.get('/product', async (req, res) => {
+    console.log(req.query.email);
+    let query = {}
+    if (req.query?.email) {
+        query = { email: req.query.email }
+    }
+    const result = await productCollection.find(query).toArray();
+    res.send(result)
+})
+
+app.get('/product/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const query = { _id: new ObjectId(id) }
+    const result = await productCollection.findOne(query)
+    res.send(result)
+})
+
+app.post('/product', async (req, res) => {
+    const product = req.body;
+    console.log(product)
+    const result = await productCollection.insertOne(product);
+    res.send(result)
+})
+
+app.put('/product/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const filter = { _id: new ObjectId(id) }
+    const options = { upsert: true };
+    const updateService = req.body;
+    const service = {
+        $set: {
+            area: updateService.area,
+            description: updateService.description,
+            photo: updateService.photo,
+            price: updateService.price,
+            service: updateService.service
+        }
+    }
+    const result = await productCollection.updateOne(filter, service, options)
+    res.send(result)
+})
+
+app.delete('/product/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const query = { _id: new ObjectId(id) }
+    const result = await productCollection.deleteOne(query)
+    res.send(result)
+})
+
+// post bookings
+
+app.get('/bookings', async (req, res) => {
+    console.log(req.query.email);
+    let query = {}
+    if (req.query?.email) {
+        query = { email: req.query.email }
+    }
+    const result = await bookingCollection.find(query).toArray();
+    res.send(result)
+})
+
+app.post('/bookings', async (req, res) => {
+    const booking = req.body;
+    console.log(booking)
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result)
+})
 
 
 
